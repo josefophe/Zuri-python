e160103356d02fcf41c5b3393f51d87e59298261
Joseph Ebin <josefophe@gmail.com>
2022-05-28 23:17:23 +0100

    Update main.py

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -6,6 +6,15 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+    
+        if len(word) == len(anagram):
+        sorted_word = sorted(word)
+        sorted_anagram = sorted(anagram)
 
+        if sorted_word == sorted_anagram:
+            return True
+        else:
+            return False
+        
     return True
 
